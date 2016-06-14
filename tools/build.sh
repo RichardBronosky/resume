ad5692b47ed68bd4c3aa0e041f7da071983cf9fb
Bruno Bronosky <Bruno@Bronosky.com>
2016-06-14 15:01:41 -0400

    Update for Docker >v1.5

--- tools/build.sh
+++ tools/build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-docker_username="$(sed 's/.*(//;s/).*//' <(docker login & sleep 0.2; kill %1))"
+docker_username="$(sed '/Username/!d;s/.*(//;s/).*//' <(docker login & sleep 0.2; kill %1))"
 docker_path="$(dirname $(find . -name Dockerfile | sed 's?^./??'))"
 docker_tag="$(basename $docker_path)"
+CMD="docker build --force-rm=true -t $docker_username/$docker_tag $docker_path"
 
-docker build --force-rm=true -t $docker_username/$docker_tag $docker_path
+echo $CMD
+$CMD
