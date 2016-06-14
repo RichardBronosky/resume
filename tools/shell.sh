2efd8df6d311b8a33ef139107ad3b67c2628ac4f
Bruno Bronosky <bruno@bronosky.com>
2016-06-14 15:01:41 -0400

    Update for Docker >v1.5

--- tools/shell.sh
+++ tools/shell.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-docker_username="$(sed 's/.*(//;s/).*//' <(docker login & sleep 0.2; kill %1))"
+docker_username="$(sed '/Username/!d;s/.*(//;s/).*//' <(docker login & sleep 0.2; kill %1))"
 docker_path="$(dirname $(find . -name Dockerfile | sed 's?^./??'))"
 docker_tag="$(basename $docker_path)"
+CMD="docker run -it -P --rm --entrypoint="/bin/bash" $docker_username/$docker_tag"
 
-docker run -it -P --rm --entrypoint="/bin/bash" $docker_username/$docker_tag
+echo $CMD
+$CMD
