7600c7e606c5e3c07946226dbb4cd4e0fd952df0
Bruno Bronosky <Bruno@Bronosky.com>
2019-12-27 09:14:42 +0100

    Update tools

--- tools/docker-build.sh
+++ tools/docker-build.sh
@@ -1,6 +1,24 @@
-#!/bin/bash -eux
+#!/bin/bash -eu
 
-docker_username="$(awk '/Username/{print $NF}' <(docker info 2>/dev/null))"
-docker_path="$(cd $(dirname $(find . -name Dockerfile | sed 's?^./??')); pwd)"
+root_of_git_repo="$(git rev-parse --show-toplevel)"
+remove_leading_dot_slash='s?^./??'
+remove_trailing_path='s?[^/]*$??'
+# See: https://stackoverflow.com/a/58795217/117471
+docker_username="$(docker-credential-$(jq -r .credsStore ~/.docker/config.json) list | jq -r '. | to_entries[] | select(.key | contains("docker.io")) | last(.value)')"
+docker_path="$(find $root_of_git_repo -name Dockerfile | sed "$remove_leading_dot_slash;$remove_trailing_path")"
 docker_tag="$(basename $docker_path)"
-docker build -t $docker_username/$docker_tag $docker_path
+
+confirm_and_run(){
+    cmd="$*"
+        cat <<NOTICE
+About to execute:
+    $cmd
+
+Press return to continue. Ctrl-C to break.
+NOTICE
+    read
+    eval "$cmd"
+}
+
+confirm_and_run "docker build -t $docker_username/$docker_tag $docker_path"
+
--- tools/latex-compile.sh
+++ tools/latex-compile.sh
@@ -1,3 +1,20 @@
 #!/bin/bash -eux
 
-docker run -i richardbronosky/latex-compiler < bruno.bronosky.resume.tex > bruno.bronosky.resume.pdf
+root_of_git_repo="$(git rev-parse --show-toplevel)"
+
+confirm_and_run(){
+    cmd="$*"
+        cat <<NOTICE
+About to execute:
+    $cmd
+
+Press return to continue. Ctrl-C to break.
+NOTICE
+    read
+    eval "$cmd"
+}
+
+for latex_file in $(find $root_of_git_repo -name *.tex); do
+    pdf_file="$(basename $latex_file .tex).pdf"
+    confirm_and_run "docker run -i richardbronosky/latex-compiler < $latex_file > $pdf_file"
+done
